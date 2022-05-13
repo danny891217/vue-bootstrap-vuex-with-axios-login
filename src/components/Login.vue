6dbd10dddd967aa1018c4ee22d3a29331c66548f
a243903 <danny.li@volvo.com>
2022-05-13 09:59:44 +0800

    logo test

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="form-signin">
     <form>
-      <img class="mb-4" src="../assets/logo.png" alt="" width="72" height="72">
+      <img class="mb-4" src="../assets/bootstrap-logo.svg" alt="" width="72" height="57">
       <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
       <div v-if="errorMsg" class="text-danger">{{errorMsg}}</div>
       <div class="form-floating">
